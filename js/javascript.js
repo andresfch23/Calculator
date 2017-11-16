e2312023b81b123398f6fb754b43a8d6c3291050
Andres Felipe Chavez Herrera <felipe.chavez@udea.edu.co>
2017-11-15 19:59:50 -0500

    hice unos cambios de estilo en CSS e inicie con la funcionalidad de los botones

--- js/javascript.js
+++ js/javascript.js
@@ -4,6 +4,32 @@ $(document).ready(function(){
 
     $containerItem.click(function(){
         var character = $(this).text();
-        $inputArea.text(character);
+        $inputArea.append(character);
+    });
+
+    $(document).keypress(function(press) {
+        var id = press.which; 
+        console.log(id);
+        if( id === 48) {
+            $inputArea.append("0");
+        } else if (id == 49) {
+            $inputArea.append("1");
+        } else if (id == 50) {
+            $inputArea.append("2");
+        } else if (id == 51) {
+            $inputArea.append("3");
+        } else if (id == 52) {
+            $inputArea.append("4");
+        } else if (id == 53) {
+            $inputArea.append("5");
+        } else if (id == 54) {
+            $inputArea.append("6");
+        } else if (id == 55) {
+            $inputArea.append("7");
+        } else if (id == 56) {
+            $inputArea.append("8");
+        } else if (id == 57) {
+            $inputArea.append("9");
+        }  
     });
 });
